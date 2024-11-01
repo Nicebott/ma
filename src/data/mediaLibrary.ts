6feb528e8ca03ab806a40996a71623411accb8f9
Nicebott <151235738+Nicebott@users.noreply.github.com>
2024-11-01 12:37:01 -0400

    Update mediaLibrary.ts

--- src/data/mediaLibrary.ts
+++ src/data/mediaLibrary.ts
@@ -22,42 +22,6 @@ export const mediaLibrary: MediaItem[] = [
             number: 1,
             title: "Piloto",
             videoUrl: "https://filemoon.sx/e/l63p1aw5kf4h",
-            thumbnail: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/beKgqwruOGmVAvRwRvfuOcXwl2Z.jpg",
-            airDate: "2021-02-23"
-          },
-          {
-            number: 2,
-            title: "Heritage",
-            videoUrl: "Próximamente",
-            thumbnail: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/beKgqwruOGmVAvRwRvfuOcXwl2Z.jpg",
-            airDate: "2021-03-02"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: 2,
-    title: "Superman & Lois",
-    originalTitle: "Superman & Lois",
-    description: "Clark Kent y Lois Lane regresan a Smallville con sus hijos Jonathan y Jordan, donde se encuentran con nuevos desafíos tanto como padres como superhéroes.",
-    type: "series",
-    rating: 8.061,
-    featured: false,
-    coverUrl: "https://image.tmdb.org/t/p/original/vlv1gn98GqMnKHLSh0dNciqGfBl.jpg",
-    videoUrl: "https://filemoon.sx/e/l63p1aw5kf4h",
-    status: "En emisión",
-    lastAirDate: "2024-10-21",
-    genres: ["Drama", "Sci-Fi & Fantasy"],
-    tmdbUrl: "https://www.themoviedb.org/tv/95057-superman-lois",
-    seasons: [
-      {
-        number: 1,
-        episodes: [
-          {
-            number: 1,
-            title: "Piloto",
-            videoUrl: "https://filemoon.sx/e/l63p1aw5kf4h",
             thumbnail: "https://image.tmdb.org/t/p/w500/vlv1gn98GqMnKHLSh0dNciqGfBl.jpg",
             airDate: "2021-02-23"
           },
@@ -135,5 +99,20 @@ export const mediaLibrary: MediaItem[] = [
         ]
       }
     ]
+  },
+  {
+    id: 2,
+    title: "Canario Negro",
+    originalTitle: "Canario Negro",
+    description: "La agente de alto rango de la CIA Avery Graves es chantajeada por terroristas para que traicione a su propio país y salve a su marido secuestrado. Separada de su equipo, recurre a sus contactos del hampa para sobrevivir y ayudar a localizar la codiciada información que quieren los secuestradores.",
+    type: "movie",
+    rating: 8.061,
+    featured: false,
+    coverUrl: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8iD8O4ZFhVHpT7RUmHJeyGEtZpX.jpg",
+    videoUrl: "https://filemoon.sx/e/9n6oq6t61qrg",
+    status: "Estrenado",
+    lastAirDate: "2024-10-21",
+    genres: ["Drama", "Sci-Fi & Fantasy"],
+    tmdbUrl: "https://www.themoviedb.org/movie/976734-canary-black"
   }
 ];
